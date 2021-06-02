1658081ae4af95aa875f5f7fde2f54bc9fb1456f
blawabar <fake.torrent@wp.pl>
2021-06-02 21:08:43 +0200

    feat: add types defintions for Character and House enitities

---
+++ src/data/types.ts
@@ -0,0 +1,37 @@
+export type Character = {
+  url: string; //	The hypermedia URL of this resource
+  name: string; // The name of this character
+  gender: string; // The gender of this character.
+  culture: string; // The culture that this character belongs to.
+  born: string; // Textual representation of when and where this character was born.
+  died: string; // Textual representation of when and where this character died.
+  titles: Array<string>; // The titles that this character holds.
+  aliases: Array<string>; // The aliases that this character goes by.
+  father: string; // The character resource URL of this character's father.
+  mother: string; // The character resource URL of this character's mother.
+  spouse: string; // Character resource URLs that has had a POV-chapter in this book.
+  allegiances: Array<string>; // An array of House resource URLs that this character is loyal to.
+  books: Array<string>; // An array of Book resource URLs that this character has been in.
+  povBooks: Array<string>; //	An array of Book resource URLs that this character has had a POV-chapter in.
+  tvSeries: Array<string>; //	An array of names of the seasons of Game of Thrones that this character has been in.
+  playedBy: Array<string>; //	An array of actor names that has played this character in the TV show Game Of Thrones.
+};
+
+export type House = {
+  url: string; //	The hypermedia URL of this resource
+  name: string; // The name of this house
+  region: string; // The region that this house resides in.
+  coatOfArms: string; // Text describing the coat of arms of this house.
+  words: string; //	The words of this house.
+  titles: Array<string>; //	The titles that this house holds.
+  seats: Array<string>; // The seats that this house holds.
+  currentLord: string; //	The Character resource URL of this house's current lord.
+  heir: string; // The Character resource URL of this house's heir.
+  overlord: string; // The House resource URL that this house answers to.
+  founded: string; //	The year that this house was founded.
+  founder: string; //	The Character resource URL that founded this house.
+  diedOut: string; //	The year that this house died out.
+  ancestralWeapons: Array<string>; //	An array of names of the noteworthy weapons that this house owns.
+  cadetBranches: Array<string>; // An array of House resource URLs that was founded from this house.
+  swornMembers: Array<string>; //	An array of Character resource URLs that are sworn to this house.
+};
